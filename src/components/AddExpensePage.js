6f9e15894fd9977ad0eb994214ab40d00f23ade9
nader <nader.hariri@hotmail.com>
2018-05-28 20:34:04 +0300

    Setup production webpack build

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,30 +1,27 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { addExpense } from '../actions/expenses'; 
+import { addExpense } from '../actions/expenses';
 
-
-// we switched to a class based component because we didnt want to have inline event handlers https://www.quora.com/What-is-event-handler-in-JavaScript
 export class AddExpensePage extends React.Component {
-    onSubmit = (expense) => {
-        this.props.addExpense(expense);
-        this.props.history.push('/');
-    };
-    render() {
-        return (
-            <div>
-            <h1>Add Expense</h1>
-            <ExpenseForm 
-                onSubmit={this.onSubmit} 
-            />
-        </div>
-        );
-    }
-
+  onSubmit = (expense) => {
+    this.props.addExpense(expense);
+    this.props.history.push('/');
+  };
+  render() {
+    return (
+      <div>
+        <h1>Add Expense</h1>
+        <ExpenseForm
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  }
 }
 
-const mapDispatchToProps = (props) => ({
-    addExpense: (expense) => dispatch(addExpense(expense))
+const mapDispatchToProps = (dispatch) => ({
+  addExpense: (expense) => dispatch(addExpense(expense))
 });
 
-export default connect(undefined, mapDispatchToProps)(AddExpensePage); 
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
